12f437ecff0fd2f0818c10f9d9c92a6613c8bf95
USUARIO <USUARIO@DESKTOP-OOM9MTA>
2025-08-19 08:12:15 -0500

    Agregar archivos de proyecto.

---
+++ VOTACIONES/Controllers/AprendizsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VOTACIONES.Models;
+
+namespace VOTACIONES.Controllers
+{
+    public class AprendizsController : Controller
+    {
+        private VOTACIONESEntities1 db = new VOTACIONESEntities1();
+
+        // GET: Aprendizs
+        public ActionResult Index()
+        {
+            return View(db.Aprendiz.ToList());
+        }
+
+        // GET: Aprendizs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aprendiz aprendiz = db.Aprendiz.Find(id);
+            if (aprendiz == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aprendiz);
+        }
+
+        // GET: Aprendizs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Aprendizs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_aprendiz,correo_aprendiz,contraseña_aprendiz")] Aprendiz aprendiz)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Aprendiz.Add(aprendiz);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aprendiz);
+        }
+
+        // GET: Aprendizs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aprendiz aprendiz = db.Aprendiz.Find(id);
+            if (aprendiz == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aprendiz);
+        }
+
+        // POST: Aprendizs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_aprendiz,correo_aprendiz,contraseña_aprendiz")] Aprendiz aprendiz)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aprendiz).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aprendiz);
+        }
+
+        // GET: Aprendizs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aprendiz aprendiz = db.Aprendiz.Find(id);
+            if (aprendiz == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aprendiz);
+        }
+
+        // POST: Aprendizs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Aprendiz aprendiz = db.Aprendiz.Find(id);
+            db.Aprendiz.Remove(aprendiz);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Seleccion()
+        {
+            return View();
+        }
+        public ActionResult Diurna()
+        {
+            return View();
+        }
+        public ActionResult Mixta()
+        {
+            return View();
+        }
+        public ActionResult Nocturna()
+        {
+            return View();
+        }
+    }
+}
---
+++ VOTACIONES/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VOTACIONES.Models;
+
+namespace VOTACIONES.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+
+        private VOTACIONESEntities1 db = new VOTACIONESEntities1();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string documento, string contrasena)
+        {
+            var admin = db.Administrador
+                .FirstOrDefault(a => a.correo_admin == documento && a.contraseña_admin == contrasena);
+
+            if (admin != null)
+            {
+                Session["Administrador"] = admin;
+                return RedirectToAction("Admin", "Administradors");
+            }
+
+            var aprendiz = db.Aprendiz
+                .FirstOrDefault(a => a.correo_aprendiz == documento && a.contraseña_aprendiz == contrasena);
+
+            if (aprendiz != null)
+            {
+                Session["Aprendiz"] = aprendiz;
+                return RedirectToAction("Seleccion", "Aprendizs");
+            }
+
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+            return View();
+        }
+
+
+
+    }
+}
